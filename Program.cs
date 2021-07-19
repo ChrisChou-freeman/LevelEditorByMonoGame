db661af7b39f3e0a58879f64e90da7fd1a4ddcd6
ChrisChou-freeman <zhou.guanwei@foxmail.com>
2021-07-19 11:44:10 +0800

    initial rep

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ActionGameExample
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game())
+                game.Run();
+        }
+    }
+}
